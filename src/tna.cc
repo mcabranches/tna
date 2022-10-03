0524ebcf53032d4e145ea0c807469a4eaae86301
Marcelo <mabranches@yahoo.com.br>
2022-10-03 15:36:07 -0600

    Modularizing TNA components 2

--- src/tna.cc
+++ src/tna.cc
@@ -7,8 +7,6 @@ int stop = 0;
 
 static void unload_prog(int sig) {
     std::cout <<"\nStopping TNA controller ..." <<  std::endl;
-    //string del_fp = "rm src/*.bpf.*";
-    //system(del_fp.c_str());
     stop = 1;
     tna_g_ns::clean_g_ns();
     return;
@@ -28,18 +26,7 @@ int main(void)
     Tnabr tnabr = Tnabr();
     tnatm.add_tnabr(&tnabr);
 
-    //tnanl.init_tna_objects(&tnatm.tnaodb);
     tnanl.init_tna_objects(&tnatm);
-
-    //TNA Tnabr object
-    //Tnabr tnabr = Tnabr(XDP_FLAGS_SKB_MODE);
-    //create_tna_bridge(&tnabr, &tnanl);
-
-    //TNA Tnaipt object - does not use tnanl (Netlink)
-    //Tnaipt tnaipt = Tnaipt();
-
-    //string build_fp = "cd ../src && cd fp_assembler/fps && fps && cd .. && make";
-    //system(build_fp.c_str());
     
     cout << "-----------------------" << endl;
     cout << "TNA main loop ..." << endl;
@@ -49,10 +36,10 @@ int main(void)
     tnatm.tna_topo_print();
     tnatm.deploy_tnafp();
 
-    while(!stop) { //controller's main loop
+    /* controller's main loop */
+    while(!stop) {
         cout << "..." << endl;
-        //process_tna_event(&tnabr, &tnanl, &tnaipt);
-        //this blocks and is awaken if an event happens
+        /* this blocks and is awaken if an event happens */
         process_tna_event(&tnanl, &tnatm);
     }
 
