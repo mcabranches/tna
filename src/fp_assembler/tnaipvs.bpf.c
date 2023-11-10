17a67ffaba0f25bf7e56ee56817183cca79b0907
Erika Hunhoff <hunhoff.erika@gmail.com>
2023-11-10 11:39:20 -0700

    Slowly reimplementing the ipvs code

--- src/fp_assembler/tnaipvs.bpf.c
+++ src/fp_assembler/tnaipvs.bpf.c
@@ -26,10 +26,52 @@
 })
 
 SEC("xdp_packet_parser")
-int xdp_prog_simple(struct xdp_md *ctx)
+int  xdp_parser_func(struct xdp_md *ctx)
 {
-        bpf_printk("...");
-        return XDP_PASS;
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	struct bpf_fib_lookup fib_params = {};
+	struct ethhdr *eth;
+	struct iphdr *iphdr;
+	struct tcphdr *tcphdr;
+	struct udphdr *udphdr;
+
+	struct bpf_ip_vs_lookup params = {
+		.in = 0
+	};
+	int len;
+	int rc;
+	/* Default action XDP_PASS, imply everything we couldn't parse, or that
+	 * we don't want to deal with, we just pass up the stack and let the
+	 * kernel deal with it.
+	 */
+	__u32 action = XDP_PASS; /* Default action */
+
+        /* These keep track of the next header type and iterator pointer */
+	struct hdr_cursor nh;
+	int nh_type, nxthdr;
+
+	/* Start next header cursor position at data start */
+	nh.pos = data;
+
+	/* Packet parsing in steps: Get each header one at a time, aborting if
+	 * parsing fails. Each helper function does sanity checking (is the
+	 * header type in the packet correct?), and bounds checking.
+	 */
+	nh_type = parse_ethhdr(&nh, data_end, &eth);
+
+	/* could handle ETH_P_IPV6 but we don't right now */
+	if (nh_type != ETH_P_IP) {
+		bpf_printk("wrong packet type? actual=%x expected=%x v6=%x\n", nh_type, ETH_P_IP, ETH_P_IPV6);
+		goto out;
+	}
+
+	//nxthdr = parse_iphdr(&nh, data_end, &iphdr);
+
+	bpf_printk("...");
+
+out:
+	return action;
 }
 
 char _license[] SEC("license") = "GPL";
